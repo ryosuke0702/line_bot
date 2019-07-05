10bd21982eeed9793fcbf65fc07f11395b3415b2
松岡亮佑 <matsuokaryousuke@matsuokukenoMBP.lan>
2019-07-05 22:17:19 +0900

    forth commit

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -28,7 +28,7 @@ class LinebotsController < ApplicationController
           when Line::Bot::Event::MessageType::Text
             message = {
               type: 'text',
-              text: event.message['text']
+              text: event.message['勉強しろ']
             }
             client.reply_message(event['replyToken'], message)
           end
